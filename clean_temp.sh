e71ccb29075ef3f0cc78795b2ee144083a28c6e4
ogonzalez <om.gonzalez007@gmail.com>
2025-09-21 07:40:00 -0500

    optimizando los comandos de busqueda de archivos

--- clean_temp.sh
+++ clean_temp.sh
@@ -1,18 +1,19 @@
 #! /bin/bash
-#
+set -euo pipefail
 #This script clean temp files that have more than 7 days
 #
 
 days=${1:-7}
 
 #Count the amount files into the /tmp
-countFiles=$(ls -l /tmp | wc -l)
-((countFiles -=  1))
+#countFiles=$(ls -l /tmp | wc -l)
+#((countFiles -=  1))
+countFiles=$(find /tmp -type f | wc -l)
 
 #display the amount and files with more seven days
 echo "El directorio /tmp actuamente tiene $countFiles archivos temporales"
 echo "De los $countFiles estos archivos tienen mas de $days"
-files=$(find /tmp -type f -mtime $days)
+files=$(find /tmp -type f -mtime +$days)
 echo $files
 
 #Clean the files
