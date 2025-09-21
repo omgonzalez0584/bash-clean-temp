1efc35e94ce31416f00a21b5416ad1afc2e9eec6
ogonzalez <om.gonzalez007@gmail.com>
2025-09-21 07:17:31 -0500

    Listando el numero y los archivos temporales que se van a borrar

--- clean_temp.sh
+++ clean_temp.sh
@@ -8,4 +8,12 @@ echo $days
 countFiles=$(ls -l /tmp | wc -l)
 ((countFiles -=  1))
 echo "El directorio /tmp actuamente tiene $countFiles archivos temporales"
-find /tmp -type f -mtime +$days
+echo "De los $countFiles estos archivos tienen mas de $days"
+files=$(find /tmp -type f -mtime $days)
+for file in $files
+do
+	echo "$file"
+done
+
+
+
