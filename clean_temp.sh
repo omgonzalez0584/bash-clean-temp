aa70d4c4ec3ad7e36f12c6adb608acad2e0eff20
ogonzalez <om.gonzalez007@gmail.com>
2025-09-21 01:11:14 -0500

    Adding variable days and search file into tmp directory

--- clean_temp.sh
+++ clean_temp.sh
@@ -2,5 +2,10 @@
 #
 #This script clean temp files that have more than 7 days
 #
-echo "this is a test"
 
+days=${1:-7}
+echo $days
+countFiles=$(ls -l /tmp | wc -l)
+((countFiles -=  1))
+echo "El directorio /tmp actuamente tiene $countFiles archivos temporales"
+find /tmp -type f -mtime +$days
